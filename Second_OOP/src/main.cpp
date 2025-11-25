82a621a866c70ffde00d1a7733d93d4cfc392b62
Alan Gomez <gomezpazalansamuel@gmail.com>
2025-11-24 19:53:36 -0600

    I start Second

---
+++ Second_OOP/src/main.cpp
@@ -0,0 +1,11 @@
+// Simple Banking System by Alan Gomez
+// Project Idea: Develop a basic banking system that 
+// allows users to create accounts, deposit, withdraw, 
+// and check balances. Each user account can be an object.
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello world!" << std::endl;
+}
